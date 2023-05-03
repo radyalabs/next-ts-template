ef1a27674a3dee07e89554aa35625d2d34f0b5ff
hasbi <ashshidiq23@gmail.com>
2023-05-03 15:13:36 +0700

    feat: implement datatable component

---
+++ src/components/base/DataTable/index.hooks.ts
@@ -0,0 +1,19 @@
+import type { KeyboardEvent } from 'react';
+
+import type { TableProps } from '@/components/base/DataTable/index.types';
+import noop from '@/utils/noop';
+
+const useDataTable = (props: TableProps) => {
+  const { page = 1, onPageChange = noop } = props || {};
+  const onSubmitPage = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onPageChange(page);
+    }
+  };
+
+  return {
+    onSubmitPage,
+  };
+};
+
+export default useDataTable;
